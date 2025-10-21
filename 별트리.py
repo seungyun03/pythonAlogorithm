64451b40c99cd639448be869d27eae8be50cbcbb
seungyoon <144211722+seungyun03@users.noreply.github.com>
2025-10-21 19:14:37 +0900

    Update 별트리.py

--- 별트리.py
+++ 별트리.py
@@ -1,7 +1,6 @@
 print("별 트리를 만들어보자")
 h = int(input("트리의 높이 입력: "))
 for i in range(1,h-2):
-       
         print(" "*((h//2)-i+1),end='')
         print('*'*(2*i-1))
         print(" "*((h//2)-i+1),end='')
