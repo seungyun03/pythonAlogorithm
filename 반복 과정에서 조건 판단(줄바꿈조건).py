f19c92e2b8593993fb7b5c2adeac65aa53372b8c
seungyoon <neoyoon04@gmail.com>
2025-10-21 18:02:54 +0900

    반복 과정에서 줄바꿈 조건추가

--- 반복 과정에서 조건 판단(줄바꿈조건).py
+++ 반복 과정에서 조건 판단(줄바꿈조건).py
@@ -1,9 +1,21 @@
+#최적화버전
+print('*를 출력합니다')
+n = int(input("몇 개를 출력할까요?"))
+w = int(input("몇 개마다 줄바꿈 할까요?"))
+for i in range(n//w): #6/4는 실수값만,6//4는 몫만
+    print('*'*w)#굳이 기본버전의  for문 사이if문을 넣을 필요없음(복잡도만 증가함)
+rest = n % w
+print("*"*rest)
+
+#기본버전12
+""""
 print('*를 출력합니다')
 n = int(input("몇 개를 출력할까요?"))
 w = int(input("몇 개마다 줄바꿈 할까요?"))
 for i in range(n):
     print('*', end='')
     if i % w == w - 1   :
-        print() 
+        print() #줄바꿈
 if n % w:
     print()
+"""
