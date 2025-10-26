41de2f357e0adfb076fbcbc50d0935b70ddde4a8
seungyoon <neoyoon04@gmail.com>
2025-10-26 18:50:27 +0900

    .

---
+++ 기본 자료구조와 배열/리스트.py
@@ -0,0 +1,32 @@
+list01 = []#빈 리스트 생성
+list02 = [1,2,3,4,5]#숫자 리스트 생성
+list03 = ["apple","banana","cherry"]#문자열 리스트 생성
+list04 = list()
+list05 = ("abc")
+list06 = list([1,2,3])
+list07 = list((1,2,3))
+list08 = list({1,2,3})
+list09 = list(range(7))#0~6까지 숫자 리스트 생성
+list10 = list(range(3,8))#3~7까지 숫자 리스트 생성
+list11 = list(range(3,13,2))#3~11까지 2씩 증가,3 5 7 9 11
+list12 = [None]*5 #[None, None, None, None, None]
+#아래는 튜플
+tuple01 = ()#빈 튜플 생성
+tuple02 = 1, #(1,)
+tuple03 = (1,)
+tuple04 = 1,2,3
+tuple05 = 1,2,3,#(1,2,3)
+tuple06 = (1,2,3)#(1,2,3)
+tuple07 = (1,2,3,)#(1,2,3) 
+tuple08 = 'A','B','C'#('A','B','C')
+v01 = (1)#이건 튜플이 아니다. 하나의 값을 가진 int형 변수
+tuple09 = tuple()#빈 튜플 생성
+tuple10 = tuple('ABC')#('A','B','C')문자열의 각 문자로부터 튜플 생성
+tuple11 = tuple([1,2,3])#(1,2,3)리스트로 부터 원소를 가져와 튜플 생성
+tuple12 = tuple({1,2,3})#(1,2,3)집합으로부터 원소를 가져와 튜플 생성
+tuple13 = tuple(range(7))#0~6까지 숫자 튜플 생성
+tuple14 = tuple(range(3,8))#3~7까지 숫자 튜플 생성
+tuple15 = tuple(range(3,13,2))#3~11까지 2씩 증가,3 5 7 9 11
+#리스트와 튜플 풀어내기
+ 
+x = [1,2,3]
